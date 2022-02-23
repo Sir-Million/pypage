c3bfd44189511ddd8437316e7ad0446d4f1bbf0c
Millionpkp <73513663+SirDevi@users.noreply.github.com>
2022-02-23 08:24:52 -0600

    updated

--- index.py
+++ index.py
@@ -14,5 +14,13 @@ def contato():
 def about():
     return render_template('about.html')
 
+@app.route('/apply')
+def apply():
+    return render_template('apply.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
